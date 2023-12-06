2d7573e9d701a3d1df9431354130a51b0219db7a
Daniel Hilz <hilzdani@gmail.com>
2023-12-06 17:07:27 +0100

    Terme 5-8 hinzugefügt

--- Boolsche_Terme(vereinfacht).py
+++ Boolsche_Terme(vereinfacht).py
@@ -10,6 +10,17 @@ def term_3():
 def term_4(a, b, c):
     return a or (not b and not c)
 
+def term_5(a, b):
+    return a and not b
+
+def term_6(c, d):
+    return c or d
+
+def term_7(a, c):
+    return not (a and c)
+
+def term_8(a, b, c):
+    return a and (b or c)
 
 
 
