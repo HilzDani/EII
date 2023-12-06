7bd4475b2125a9f87a3070aecf36f77e84cc1e91
Daniel Hilz <hilzdani@gmail.com>
2023-12-06 17:08:35 +0100

    Tests der Terme hinzugefügt

--- Boolsche_Terme(vereinfacht).py
+++ Boolsche_Terme(vereinfacht).py
@@ -23,4 +23,18 @@ def term_8(a, b, c):
     return a and (b or c)
 
 
+# Testen der Terme mit Beispielen
+a = True
+b = False
+c = True
+d = False
+
+print(term_1(a, b))  # Ausgabe: False
+print(term_2(a, b, c))  # Ausgabe: True
+print(term_3())  # Ausgabe: False
+print(term_4(a, b, c))  # Ausgabe: True
+print(term_5(a, b))  # Ausgabe: True
+print(term_6(c, d))  # Ausgabe: True
+print(term_7(a, c))  # Ausgabe: False
+print(term_8(a, b, c))  # Ausgabe: True
 
